75ca3943f1032db7c2fafc39261cc882400a2b09
ambar <ambar.lee@gmail.com>
2021-09-20 09:59:04 +0800

    chore: rename bin file

--- gogen/cli.js
+++ gogen/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const isInSourceRepo = require('fs').existsSync(
-  require('path').join(__dirname, '.gitignore')
+  require('path').join(__dirname, '.gitkeep')
 )
 
 require(isInSourceRepo ? './lib' : './dist').run(process.argv.slice(2))
