ff6120a3a73933bbc4f4366ba9a3e8b74b85777e
ambar <ambar.lee@gmail.com>
2021-09-25 11:08:27 +0800

    test: mock username for ci

--- gogen-pkg/.gogenrc.js
+++ gogen-pkg/.gogenrc.js
@@ -5,7 +5,7 @@ module.exports = async (
   {src, dest, pipeline, template, install, gitInit},
   {name}
 ) => {
-  const username = sh('git config user.name')
+  const username = process.env.CI ? 'unknown' : sh('git config user.name')
   await pipeline(src('template/**'), template({name, username}), dest())
   await install()
   await gitInit()
