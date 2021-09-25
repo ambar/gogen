cb0b3499cd0b22aeb0798581e1aa0a635f61d013
ambar <ambar.lee@gmail.com>
2021-09-25 12:41:31 +0800

    test: add mock spec

--- gogen/test/cli.spec.js
+++ gogen/test/cli.spec.js
@@ -3,11 +3,24 @@ const glob = require('glob')
 const {mock, run} = require('../lib')
 const createTempDir = require('../lib/utils/createTempDir')
 
-describe('index', () => {
+describe('mock', () => {
   test('should throw errors', async () => {
     await expect(mock('', '')).rejects.toThrow()
     await expect(mock('foo', '')).rejects.toThrow()
     await expect(mock('', 'foo')).rejects.toThrow()
+  })
+
+  test('mock ok', async () => {
+    const generator = path.resolve(__dirname, 'fixtures/test-basic')
+    const {files} = await mock(generator, 'mylib')
+    expect(files).toMatchInlineSnapshot(`
+Array [
+  ".gitignore",
+  "README.md",
+  "index.js",
+  "package.json",
+]
+`)
   })
 })
 
